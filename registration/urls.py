f8d72271161f49ba96a5343ac43bb91235ba6efa
reSetRoot <wei860925@gmail.com>
2020-08-09 00:28:32 +0800

    Completed designed the index page.

---
+++ registration/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('accounts/', include([
+        # path('sign_up', ),
+        # path('login', ),
+        # path('logout', ),
+    ]))
+]
